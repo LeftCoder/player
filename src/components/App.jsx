6764b28c6cc6495e6543b50e8f0a26e8dafba293
Kamil Graczyk <kgraczyk@przychodnia.local>
2023-05-16 00:26:20 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,12 @@ const parseExtension = (filename) => {
 }
 
 const makeSources = (files) => {
-  const sources = files.map(file => ({
+  return files.map(file => ({
     sources: [{
       src: path + file,
       type: 'video/' + parseExtension(file)
     }]
   }))
-
-  return sources
 }
 
 const App = ({files}) => {
